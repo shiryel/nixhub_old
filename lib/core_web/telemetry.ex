ee2812cea5594476df024a71c074940c062eb704
Shiryel <contact@shiryel.com>
2022-11-26 02:17:30 -0300

    chore: update telemetry

--- lib/core_web/telemetry.ex
+++ lib/core_web/telemetry.ex
@@ -23,58 +23,64 @@ defmodule CoreWeb.Telemetry do
 
   def metrics do
     [
-      # Phoenix Metrics
-      summary("phoenix.endpoint.start.system_time",
-        unit: {:native, :millisecond}
-      ),
-      summary("phoenix.endpoint.stop.duration",
-        unit: {:native, :millisecond}
-      ),
-      summary("phoenix.router_dispatch.start.system_time",
-        tags: [:route],
-        unit: {:native, :millisecond}
-      ),
-      summary("phoenix.router_dispatch.exception.duration",
-        tags: [:route],
-        unit: {:native, :millisecond}
-      ),
-      summary("phoenix.router_dispatch.stop.duration",
-        tags: [:route],
-        unit: {:native, :millisecond}
-      ),
-      summary("phoenix.socket_connected.duration",
-        unit: {:native, :millisecond}
-      ),
-      summary("phoenix.channel_join.duration",
-        unit: {:native, :millisecond}
-      ),
-      summary("phoenix.channel_handled_in.duration",
-        tags: [:event],
+      # LiveView Metrics
+      counter("phoenix.live_view.mount.start.system_time"),
+      summary("phoenix.live_view.mount.stop.duration",
         unit: {:native, :millisecond}
       ),
 
+      # Phoenix Metrics
+      # summary("phoenix.endpoint.start.system_time",
+      #  unit: {:native, :millisecond}
+      # ),
+      # summary("phoenix.endpoint.stop.duration",
+      #  unit: {:native, :millisecond}
+      # ),
+      # summary("phoenix.router_dispatch.start.system_time",
+      #  tags: [:route],
+      #  unit: {:native, :millisecond}
+      # ),
+      # summary("phoenix.router_dispatch.exception.duration",
+      #  tags: [:route],
+      #  unit: {:native, :millisecond}
+      # ),
+      # summary("phoenix.router_dispatch.stop.duration",
+      #  tags: [:route],
+      #  unit: {:native, :millisecond}
+      # ),
+      summary("phoenix.socket_connected.duration",
+        unit: {:native, :millisecond}
+      ),
+      # summary("phoenix.channel_join.duration",
+      #  unit: {:native, :millisecond}
+      # ),
+      # summary("phoenix.channel_handled_in.duration",
+      #  tags: [:event],
+      #  unit: {:native, :millisecond}
+      # ),
+
       # Database Metrics
-      summary("core.repo.query.total_time",
-        unit: {:native, :millisecond},
-        description: "The sum of the other measurements"
-      ),
-      summary("core.repo.query.decode_time",
-        unit: {:native, :millisecond},
-        description: "The time spent decoding the data received from the database"
-      ),
-      summary("core.repo.query.query_time",
-        unit: {:native, :millisecond},
-        description: "The time spent executing the query"
-      ),
-      summary("core.repo.query.queue_time",
-        unit: {:native, :millisecond},
-        description: "The time spent waiting for a database connection"
-      ),
-      summary("core.repo.query.idle_time",
-        unit: {:native, :millisecond},
-        description:
-          "The time the connection spent waiting before being checked out for the query"
-      ),
+      # summary("core.repo.query.total_time",
+      #  unit: {:native, :millisecond},
+      #  description: "The sum of the other measurements"
+      # ),
+      # summary("core.repo.query.decode_time",
+      #  unit: {:native, :millisecond},
+      #  description: "The time spent decoding the data received from the database"
+      # ),
+      # summary("core.repo.query.query_time",
+      #  unit: {:native, :millisecond},
+      #  description: "The time spent executing the query"
+      # ),
+      # summary("core.repo.query.queue_time",
+      #  unit: {:native, :millisecond},
+      #  description: "The time spent waiting for a database connection"
+      # ),
+      # summary("core.repo.query.idle_time",
+      #  unit: {:native, :millisecond},
+      #  description:
+      #    "The time the connection spent waiting before being checked out for the query"
+      # ),
 
       # VM Metrics
       summary("vm.memory.total", unit: {:byte, :kilobyte}),
